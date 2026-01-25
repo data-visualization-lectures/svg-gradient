8c639fbe34b6f79ab5cc915f4822edbeb4babb10
Yuichi Yazaki <visualizingjp@gmail.com>
2026-01-25 19:28:09 +0900

    共通ヘッダーを絶対パスへ

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,8 @@
 
 import { defineConfig } from 'vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
     base: './',
-    plugins: [
-        viteStaticCopy({
-            targets: [
-                {
-                    src: 'supabase.js',
-                    dest: '.'
-                },
-                {
-                    src: 'dataviz-auth-client.js',
-                    dest: '.'
-                }
-            ]
-        })
-    ],
     build: {
         outDir: 'docs',
         emptyOutDir: true,
